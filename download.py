6cafb8347f342630e24225a6c5777772ebbe9b51
vacabun <maguotong66@gmail.com>
2023-02-25 22:01:36 +0800

    upload code.

---
+++ download.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+import csv
+import yt_dlp
+
+ydl_opts = {}
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+save_path = current_directory + '/save'
+
+with open('./download_list.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        title = row[0]
+        url = row[1]
+        print('save title: ' + row[0])
+        print('url: ' + row[1])
+        ydl_opts = {
+            'format': 'mp4/b',
+            'paths': {'home': save_path},
+            'outtmpl': title}
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        ydl.download([url])
